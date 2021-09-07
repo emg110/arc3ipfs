93aabe2ea8ed7289d88f9e53eb7a3361f8ec0bfb
Mohammad Ghiasi <emg110@gmail.com>
2021-09-07 13:25:00 +0430

    - changed config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    pinataApiKey: "",
-    pinataSecretApiKey: "",
+    pinataApiKey: "cc5751d62a904d462d35",
+    pinataSecretApiKey: "c34be406e8dbce0d1b2ff5260503395a3297ac3b2e76ecead0b851d2fc953f1d",
     pinataFileUrl: "https://api.pinata.cloud/pinning/pinFileToIPFS",
     pinataJSONUrl: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
     arc3MetadataJSON: {
