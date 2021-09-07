70804cfcff2aa53ceaf6b56272ba81801a4f3d35
Mohammad Ghiasi <emg110@gmail.com>
2021-09-07 18:52:40 +0430

    - added NFT sample file from diagram of proposition - added folder structure from node fs for scenario 2

--- js/arc3-ipfs.js
+++ js/arc3-ipfs.js
@@ -1,9 +1,11 @@
+const fs = require('fs');
 const bs58 = require("bs58");
 const config = require("./config")
 const pinataSDK = require('@pinata/sdk');
 const pinataApiKey = config.pinataApiKey;
 const pinataSecretApiKey = config.pinataSecretApiKey;
-const pinata = pinataSDK(pinataApiKey,pinataSecretApiKey);
+const pinata = pinataSDK(pinataApiKey, pinataSecretApiKey);
+const readableStreamForFile = fs.createReadStream('./yourfile.png');
 
 pinata.testAuthentication().then((result) => {
   console.log(result);
@@ -23,16 +25,24 @@ const convertByte32ToIpfsCidV0 = (str) => {
 };
 
 const scenario1 = (fileBlob, fileName, assetName, assetDesc) => {
+  const readableStreamForFile = fs.createReadStream('./images/ASA_IPFS scenarios');
+  let fileCat = 'image';
 
 
-  
   let fileNameSplit = fileName.split('.')
-  
+  let ext = fileNameSplit[1];
+  if (ext === 'txt') {
+    fileCat = 'text'
+  } else if (ext === 'mp4') {
+    fileCat = 'video'
+  } else if (ext === 'mp3' || ext === 'wav') {
+    fileCat = 'audio'
+  }
   let properties = {
     "file_name": fileNameSplit[0],
-    "file_extension": fileNameSplit[1],
+    "file_extension": ext,
     "file_size": fileBlob.size,
-    "file_category": "image"
+    "file_category": fileCat
   }
 
   const pinataMetadata = {
@@ -40,28 +50,36 @@ const scenario1 = (fileBlob, fileName, assetName, assetDesc) => {
     keyvalues: properties
   };
 
-  let metadata = config.arc3MetadataJson
 
-  metadata.properties = properties;
-  metadata.name = assetName;
-  metadata.description = assetDesc;
-  metadata.image = "";
-  metadata.image_integrity = "";
 
   const pinataOptions = {
     cidVersion: 0,
   };
-  
+
   const options = {
     pinataMetadata: pinataMetadata,
     pinataOptions: pinataOptions
   };
+  return pinata.pinFileToIPFS(fileBlob, options).then((resultFile) => {
+    console.log(resultFile);
+    let metadata = config.arc3MetadataJson
 
-  return pinata.pinFileToIPFS(fileBlob, options).then((result) => {
-      console.log(result);
+    metadata.properties = properties;
+    metadata.name = assetName;
+    metadata.description = assetDesc;
+    metadata.image = 'ipfs://' + resultFile.IpfsHash;
+    metadata.image_integrity = resultFile.IpfsHash;
+    return pinata.pinJSONToIPFS(metadata, options).then((resultMeta) => {
+      console.log(resultMeta);
+
+    }).catch((err) => {
+      return console.log(err);
+    });
   }).catch((err) => {
-      console.log(err);
+    return console.log(err);
   });
+
+
 };
 
 const scenario2 = (fileBlob, fileName, assetName, assetDesc) => {
@@ -73,7 +91,7 @@ const scenario2 = (fileBlob, fileName, assetName, assetDesc) => {
   metadata.image_integrity = "";
 
   let fileNameSplit = fileName.split('.');
-  
+
   let properties = {
     "file_name": fileNameSplit[0],
     "file_extension": fileNameSplit[1],
@@ -86,22 +104,32 @@ const scenario2 = (fileBlob, fileName, assetName, assetDesc) => {
     keyvalues: properties
   };
 
-  const body = metadata;
+
 
   const pinataOptions = {
     cidVersion: 0,
   };
 
   const options = {
-      pinataMetadata: pinataMetadata,
-      pinataOptions: pinataOptions
+    pinataMetadata: pinataMetadata,
+    pinataOptions: pinataOptions
   };
+  let data = JSON.stringify(student);
+  fs.writeFileSync('student-2.json', data);
+  fs.writeFileSync(fileName, fileBlob);
+  return fs.mkdir(`./${assetName}`, function (err) {
+    if (err) {
+      return console.log(err)
+    } else {
+      return pinata.pinFromFS(`./${assetName}`, options).then((result) => {
+        return console.log(result);
+      }).catch((err) => {
+        return console.log(err);
+      });
+    }
+  })
 
-  return pinata.pinJSONToIPFS(body, options).then((result) => {
-      console.log(result);
-  }).catch((err) => {
-      console.log(err);
-  });
+
 
 };
 module.exports = {
